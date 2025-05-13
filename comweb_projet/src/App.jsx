4ba8f6d16fbafccf9285ca898e6fbeecd13b94d7
maxime.ung <{mung001@ensc.fr}>
2025-05-13 14:26:58 +0200

    App Simon

--- comweb_projet/src/App.jsx
+++ comweb_projet/src/App.jsx
@@ -8,28 +8,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="conteneur">
+        <form className="formulaire">
+          <label htmlFor="username">Nom d'utilisateur</label>
+          <input type="text" id="username" name="username" placeholder="Entrez votre nom" />
+
+          <label htmlFor="password">Mot de passe</label>
+          <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe" />
+
+          <button type="submit">Envoyer</button>
+        </form>
+      </main>
     </>
   )
 }
 
 export default App
+
+
